194fc303ecfe5090261491bbf8a4c0168ef12fc5
Zvika Haramaty <haramaty.zvika@gmail.com>
2020-07-19 16:01:46 +0300

    added tools/strings_export.py and scripts_strings.csv

---
+++ tools/strings_export.py
@@ -0,0 +1,24 @@
+# export scripts strings (except 'said's) to csv
+
+import re
+import os
+import glob
+import csv
+
+INPUT_FILES = r"C:\Zvika\ScummVM-dev\SCI-Decompilation-Archive\sq3\src\*.sc"
+OUTPUT_FILE = r"C:\Zvika\ScummVM-dev\HebrewAdventure\sq3\patches\scripts_strings.csv"
+KEYS = ('filename', 'idx', 'original', 'translation')
+
+with open(OUTPUT_FILE, 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, fieldnames=KEYS)
+    dict_writer.writeheader()
+
+    for filename in glob.iglob(INPUT_FILES):
+        with open(filename) as f:
+            basename = os.path.basename(filename)
+
+            text = f.read().replace('\n', '\t')
+            entries = re.findall(r'\{.*?\}', text)
+
+            for idx, entry in enumerate(entries):
+                dict_writer.writerow({KEYS[0]: basename, KEYS[1]: idx, KEYS[2]: entry.strip('{}'), KEYS[3]: ''})
