709f9485b656110abd900f588d445a2eed9d8358
Zvika Haramaty <haramaty.zvika@gmail.com>
2020-08-23 18:04:16 +0300

    vocab_import: refactor into read_csv_file() and write_vocab_file()

--- tools/vocab_import.py
+++ tools/vocab_import.py
@@ -29,7 +29,7 @@ def get_class(l):
     return result
 
 
-def vocab_import():
+def read_csv_file():
     with open(INPUT_FILE, newline='') as csvfile:
         vocab = [{k: v for k, v in row.items()}
                  for row in csv.DictReader(csvfile, skipinitialspace=True)]
@@ -37,27 +37,52 @@ def vocab_import():
     words = []
     # group number to be used for new entries without group number, start after current maximum
     next_group = max([int(e['group']) for e in vocab if e['group'] != '']) + 1
+
+    entries = []
+    for entry in vocab:
+        new_entry = {}
+        new_entry['cls'] = get_class(entry['class'])
+        if entry['group'] == '':
+            new_entry['group'] = next_group
+            next_group += 1
+        else:
+            new_entry['group'] = int(entry['group'])
+
+        new_entry['words'] = []
+        for w in entry['words'].split('|'):
+            word = w.strip()
+            if word == '':
+                # empty word, it's OK - just a redundant |
+                pass
+            elif len(word.split()) > 1:
+                print("Warning: multy word '%s' at: " % word.strip())
+                print(entry)
+            elif word not in words:
+                words.append(word)
+                new_entry['words'].append(word)
+            else:
+                print("Warning: duplicate word '%s' at: " % word.strip())
+                print(entry)
+
+        entries.append(new_entry)
+    return entries
+
+def write_vocab_file(entries):
     binary_vocab = [0x86, 0x00]  # vocab signature
     # vocab.900 starts with 255 16-bit pointers
     # they aren't interesting...
     binary_vocab.extend([0] * (255 * 2))
-    for entry in vocab:
-        cls = get_class(entry['class'])
-        if entry['group'] == '':
-            group = next_group
-            next_group += 1
-        else:
-            group = int(entry['group'])
-        byte1 = cls >> 4
-        byte2 = (cls & 0x0f) << 4
-        byte2 += group >> 8
-        byte3 = group & 0xff
 
-        for w in entry['words'].split('|'):
+    for entry in entries:
+        byte1 = entry['cls'] >> 4
+        byte2 = (entry['cls'] & 0x0f) << 4
+        byte2 += entry['group'] >> 8
+        byte3 = entry['group'] & 0xff
+
+        for word in entry['words']:
             # don't bother with the useless compression
             binary_vocab.append(0)
 
-            word = w.strip()
             chars = str.encode(word, 'windows-1255')
             for char in chars:
                 assert char >= 0 and char <= 255
@@ -67,19 +92,15 @@ def vocab_import():
             binary_vocab.append(byte2)
             binary_vocab.append(byte3)
 
-            if word == '':
-                # empty word, it's OK - just a redundant |
-                pass
-            elif len(word.split()) > 1:
-                print("Warning: multy word '%s' at: " % word.strip())
-                print(entry)
-            elif word not in words:
-                words.append(word)
-            else:
-                print("Warning: duplicate word '%s' at: " % word.strip())
-                print(entry)
+
     with open(OUTPUT_FILE, "wb") as out_file:
         out_file.write(bytes(binary_vocab))
+
+
+def vocab_import():
+    entries = read_csv_file()
+    write_vocab_file(entries)
+
 
 if __name__ == "__main__":
     vocab_import()
